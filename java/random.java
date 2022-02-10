46624d65f63011a8e51f8bcc9a5b3454a246788e
Overkill10117 <77133161+Overkill10117@users.noreply.github.com>
2022-02-10 13:51:20 +0800

    Create random.java

---
+++ java/random.java
@@ -0,0 +1,90 @@
+public class random {
+    public static void main (String[]args)
+    {
+            double meh = Math.random() * 10;
+
+            switch ((int) meh) {
+                case 1 -> {
+                    System.out.print("It is certain");
+                    return;
+                }
+                case 2 -> {
+                    System.out.print(" It is decidedly so");
+                    return;
+                }
+                case 3 -> {
+                    System.out.print(" Without a doubt");
+                    return;
+                }
+                case 4 -> {
+                    System.out.print("Yes definitely");
+                    return;
+                }
+                case 5 -> {
+                    System.out.print("You may rely on it");
+                    return;
+                }
+                case 6 -> {
+                    System.out.print("As I see it, yes");
+                    return;
+                }
+                case 7 -> {
+                    System.out.print("Most likely");
+                    return;
+                }
+                case 8 -> {
+                    System.out.print("Outlook good");
+                    return;
+                }
+                case 9 -> {
+                    System.out.print("Yes");
+                    return;
+                }
+                case 10 -> {
+                    System.out.print("Signs point to yes");
+                    return;
+                }
+                case 11 -> {
+                    System.out.print("Reply hazy, try again");
+                    return;
+                }
+                case 12 -> {
+                    System.out.print("Ask again later");
+                    return;
+                }
+                case 13 -> {
+                    System.out.print("Better not tell you now");
+                    return;
+                }
+                case 14 -> {
+                    System.out.print("Cannot predict now");
+                    return;
+                }
+                case 15 -> {
+                    System.out.print("Concentrate and ask again");
+                    return;
+                }
+                case 16 -> {
+                    System.out.print("Don't count on it");
+                    return;
+                }
+                case 17 -> {
+                    System.out.print("My reply is no");
+                    return;
+                }
+                case 18 -> {
+                    System.out.print("My sources say no");
+                    return;
+                }
+                case 19 -> {
+                    System.out.print("Outlook not so good");
+                    return;
+                }
+                case 20 -> {
+                    System.out.print("Very doubtful");
+                    return;
+                }
+            }
+    }
+}
+
